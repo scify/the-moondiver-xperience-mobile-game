24b58ad505a7c3a864639e9d769a48058a83195d
Pavlos Isaris <paulisaris@gmail.com>
2018-01-24 16:15:28 +0200

    Deleted MoonwalkerPlayer class and unified it with Player class

--- core/src/org/scify/moonwalker/app/MoonWalkerGameState.java
+++ core/src/org/scify/moonwalker/app/MoonWalkerGameState.java
@@ -1,38 +1,25 @@
 package org.scify.moonwalker.app;
 
 import com.badlogic.gdx.physics.box2d.World;
-
 import org.scify.engine.GameEvent;
-import org.scify.engine.Renderable;
 import org.scify.engine.GameState;
-import org.scify.moonwalker.app.actors.MoonWalkerPlayer;
+import org.scify.moonwalker.app.actors.Player;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class MoonWalkerGameState extends GameState {
 
     public World world;
-    protected MoonWalkerPlayer player;
-    protected List<Renderable> renderableList = new ArrayList<Renderable>();
+    protected Player player;
 
-    public MoonWalkerGameState(List<GameEvent> eventQueue, MoonWalkerPlayer player, World world) {
+    public MoonWalkerGameState(List<GameEvent> eventQueue, Player player, World world) {
         super(eventQueue);
         this.player = player;
         this.world = world;
         this.renderableList.add(player);
     }
 
-    public List<Renderable> getRenderableList() {
-        return renderableList;
-    }
-
-    @Override
-    public List<GameEvent> getEventQueue() {
-        return eventQueue;
-    }
-
-    public MoonWalkerPlayer getPlayer() {
+    public Player getPlayer() {
         return player;
     }
 
