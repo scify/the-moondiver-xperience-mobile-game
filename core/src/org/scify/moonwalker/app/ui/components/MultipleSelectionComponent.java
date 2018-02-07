e0dab5436dc07eb3d2db90f900c187b3818d216d
Paul Isaris <paulisaris@gmail.com>
2018-02-07 14:27:03 +0200

    Refactored component to extend Group class so that it can support multiple child components.

--- core/src/org/scify/moonwalker/app/ui/components/MultipleSelectionComponent.java
+++ core/src/org/scify/moonwalker/app/ui/components/MultipleSelectionComponent.java
@@ -2,15 +2,18 @@ package org.scify.moonwalker.app.ui.components;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Align;
 import org.scify.moonwalker.app.helpers.GameInfo;
 import org.scify.moonwalker.app.helpers.ResourceLocator;
 import org.scify.moonwalker.app.ui.input.UserInputHandlerImpl;
 
 
-public class MultipleSelectionComponent extends Table {
+public class MultipleSelectionComponent extends Group {
 
     protected String relativeAvatarPath;
     protected Image avatarImg;
@@ -18,44 +21,52 @@ public class MultipleSelectionComponent extends Table {
     protected String questionText;
     protected ResourceLocator resourceLocator;
     protected GameInfo gameInfo;
+    protected Table table;
 
     public MultipleSelectionComponent(String labelTxt, String relativeAvatarPath) {
+        this.resourceLocator = new ResourceLocator();
         this.relativeAvatarPath = relativeAvatarPath;
         this.questionText = labelTxt;
         this.gameInfo = GameInfo.getInstance();
+        setWidth(gameInfo.getScreenWidth());
     }
 
     public void initActor(Skin skin) {
+        table = new Table();
+        //table.debug();
+        table.center().bottom().padBottom(40);
         setHeight(300);
-        //debug();
-        center().bottom().padBottom(40);
-        setSkin(skin);
-        setFillParent(true);
+        table.setSkin(skin);
+        table.setFillParent(true);
         if(questionText != null)
             addLabelToTable();
         if(relativeAvatarPath != null)
             addAvatarToTable();
+        Image img = new Image(new TextureRegionDrawable(new TextureRegion(new Texture(resourceLocator.getFilePath("img/component_background.png")))));
+        img.setSize(gameInfo.getScreenWidth(), gameInfo.getScreenHeight() / 2f);
+        addActor(img);
+        addActor(table);
     }
 
     public void addButton(String btnTitle, UserInputHandlerImpl inputHandler, int btnIndex) {
         int alignment = Align.left;
         if(btnIndex % 2 == 0)
             alignment = Align.right;
-        TextButton newBtn = new TextButton(btnTitle, getSkin());
+        TextButton newBtn = new TextButton(btnTitle, table.getSkin());
         newBtn.padBottom(5).padTop(5).padLeft(10).padRight(10);
         newBtn.addListener(inputHandler);
         // set the width as 1/2 of the screen, minus 40 pixels margin
-        add(newBtn).width(gameInfo.getScreenWidth() * 0.5f - 40).align(alignment).pad(10);
+        table.add(newBtn).width(gameInfo.getScreenWidth() * 0.5f - 40).align(alignment).pad(10);
         if(btnIndex % 2 == 0) {
-            row();
+            table.row();
         }
     }
 
     private void addLabelToTable() {
-        Label titleLabel = new Label(questionText, getSkin());
+        Label titleLabel = new Label(questionText, table.getSkin());
         titleLabel.setWrap(true);
-        add(titleLabel).align(Align.center).padBottom(15);
-        row();
+        table.add(titleLabel).align(Align.center).padBottom(15);
+        table.row();
     }
 
     private void addAvatarToTable() {
@@ -63,6 +74,6 @@ public class MultipleSelectionComponent extends Table {
         avatarSprite.setSize((float) (gameInfo.getScreenWidth() * 0.15), (float) (gameInfo.getScreenWidth() * 0.1));
         avatarImg = new Image(new SpriteDrawable(avatarSprite));
 
-        add(avatarImg).width(gameInfo.getScreenWidth() * 0.2f).padLeft(5).padBottom(10);
+        table.add(avatarImg).width(gameInfo.getScreenWidth() * 0.2f).padLeft(5).padBottom(10);
     }
 }
