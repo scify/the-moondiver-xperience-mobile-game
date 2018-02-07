9e1e9af3eeda1b41597ec96e6b15988ff69d31a9
Paul Isaris <paulisaris@gmail.com>
2018-02-07 11:30:02 +0200

    Set custom dimensions for desktop launcher

--- desktop/src/org/scify/moonwalker/app/desktop/DesktopLauncher.java
+++ desktop/src/org/scify/moonwalker/app/desktop/DesktopLauncher.java
@@ -3,11 +3,13 @@ package org.scify.moonwalker.app.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import org.scify.moonwalker.app.MoonWalker;
-import org.scify.moonwalker.app.helpers.GameInfo;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		// Custom dimensions for desktop
+		config.height = 411;
+		config.width = 731;
 		new LwjglApplication(new MoonWalker(), config);
 	}
 }
