cc5e54ce547a1ac5740953b560c181d0fcd2d82c
Paul Isaris <paulisaris@gmail.com>
2018-02-09 16:56:37 +0200

    Added functionality to spawn a calculator episode

--- core/src/org/scify/moonwalker/app/game/scenarios/MoonWalkerScenario.java
+++ core/src/org/scify/moonwalker/app/game/scenarios/MoonWalkerScenario.java
@@ -1,16 +1,19 @@
 package org.scify.moonwalker.app.game.scenarios;
 
-import org.scify.engine.Episode;
-import org.scify.engine.RenderingEngine;
-import org.scify.engine.Scenario;
-import org.scify.engine.UserInputHandler;
+import org.scify.engine.*;
+import org.scify.moonwalker.app.game.episodes.CalculatorEpisode;
 import org.scify.moonwalker.app.game.episodes.KnightRaceEpisode;
 import org.scify.moonwalker.app.game.episodes.RoomEpisode;
 
 public class MoonWalkerScenario extends Scenario {
 
+    protected RenderingEngine renderingEngine;
+    protected UserInputHandler userInputHandler;
+
     public MoonWalkerScenario(RenderingEngine renderingEngine, UserInputHandler userInputHandler) {
         super();
+        this.renderingEngine = renderingEngine;
+        this.userInputHandler = userInputHandler;
         Episode firstEpisode = new RoomEpisode(renderingEngine, userInputHandler);
         setFirstEpisode(firstEpisode);
         appendEpisode(new KnightRaceEpisode(renderingEngine, userInputHandler));
@@ -20,11 +23,21 @@ public class MoonWalkerScenario extends Scenario {
     // EpisodeEndState is class, containing the game state instance
     // add ScenarioState class with game state field
 
-     // TODO override getNextEpisode
-    // if episode endstate is calculator
-    // add calculator episode and re-add previous episode
-    // else call parent
-    // or add calculator episode as temp episode
-    // without changing the current episode
-    // and when calculator episode ends, re-run current episode
+    @Override
+    protected Episode getNextEpisode(EpisodeEndState state) {
+        EpisodeEndStateCode endStateCode = state.getEndStateCode();
+        switch (endStateCode) {
+            case CALCULATOR_STARTED:
+                // or add calculator episode as temp episode
+                // without changing the current episode
+                Episode calculatorEpisode = new CalculatorEpisode(renderingEngine, userInputHandler, state.getGameState());
+                return calculatorEpisode;
+            case CALCULATOR_FINISHED:
+                //when calculator episode ends, re-run current episode
+                break;
+            default:
+                break;
+        }
+        return super.getNextEpisode(state);
+    }
 }
