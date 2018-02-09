406a36633422630cc1a7f1f73a66225d0a8db27b
Paul Isaris <paulisaris@gmail.com>
2018-02-09 16:16:50 +0200

    Added javadoc

--- core/src/org/scify/moonwalker/app/game/episodes/CalculatorEpisode.java
+++ core/src/org/scify/moonwalker/app/game/episodes/CalculatorEpisode.java
@@ -3,6 +3,13 @@ package org.scify.moonwalker.app.game.episodes;
 import org.scify.engine.*;
 import org.scify.moonwalker.app.game.rules.episodes.CalculatorEpisodeRules;
 
+/**
+ * This is a self-contained episode (meaning that it usually gets invoked
+ * by another episode), presenting a simple calculator to the user.
+ * The constructor takes a {@link GameState} instance as an argument
+ * in order to set the already defined (in another episode) game state
+ * to the rules.
+ */
 public class CalculatorEpisode extends Episode<EpisodeEndState>{
 
     public CalculatorEpisode(RenderingEngine renderingEngine, UserInputHandler userInputHandler, GameState gsCurrent) {
