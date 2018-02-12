73a38d5a60dd4959645b5308a010d62f7abba547
Paul Isaris <paulisaris@gmail.com>
2018-02-12 12:55:34 +0200

    Initialize height and width for gameInfo instance

--- core/src/org/scify/moonwalker/app/MoonWalker.java
+++ core/src/org/scify/moonwalker/app/MoonWalker.java
@@ -2,12 +2,17 @@ package org.scify.moonwalker.app;
 
 import com.badlogic.gdx.Game;
 
+import com.badlogic.gdx.Gdx;
+import org.scify.moonwalker.app.helpers.GameInfo;
 import org.scify.moonwalker.app.screens.GameLauncher;
 
 public class MoonWalker extends Game {
 
     @Override
     public void create() {
+        GameInfo gameInfo = GameInfo.getInstance();
+        gameInfo.setScreenWidth(Gdx.graphics.getWidth());
+        gameInfo.setScreenHeight(Gdx.graphics.getHeight());
         this.setScreen(new GameLauncher(this));
     }
 
