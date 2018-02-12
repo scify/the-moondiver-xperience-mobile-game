b22debee1040504a50725b5c2dd394d52063aac2
Paul Isaris <paulisaris@gmail.com>
2018-02-12 16:00:54 +0200

    Added method for cleaning up the state instance

--- core/src/org/scify/engine/Rules.java
+++ core/src/org/scify/engine/Rules.java
@@ -3,9 +3,17 @@ package org.scify.engine;
 public interface Rules<StateType, Action, EndStateType> {
 
     StateType getInitialState();
-    StateType getNextState(StateType gsCurrent, Action userAction);
-    boolean isGameFinished(StateType gsCurrent);
-    boolean isGamePaused(StateType gsCurrent);
+    StateType getNextState(StateType currentState, Action userAction);
+    boolean isGameFinished(StateType currentState);
+    boolean isGamePaused(StateType currentState);
     void disposeResources();
-    EndStateType determineEndState(StateType gsCurrent);
+    EndStateType determineEndState(StateType currentState);
+    /**
+     * Removes from game state any {@link GameEvent}s that
+     * we want to be repeated if this episode is started again (resumed)
+     * For example, If we want the conversation to happen again,
+     * we should remove all conversation-relevant {@link GameEvent}s
+     * @param currentState the current {@link GameState} instance
+     */
+    void cleanUpState(StateType currentState);
 }
