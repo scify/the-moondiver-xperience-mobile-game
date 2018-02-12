e662327361f9bcb4d6439cbf785730278ed78472
Paul Isaris <paulisaris@gmail.com>
2018-02-12 16:03:41 +0200

    Refactored playEpisode method to be independent from the currentEpisode field. Added lastEpisode and method to set the current episode.

--- core/src/org/scify/engine/Scenario.java
+++ core/src/org/scify/engine/Scenario.java
@@ -1,33 +1,31 @@
 package org.scify.engine;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public abstract class Scenario {
 
     protected Episode currentEpisode;
     protected Map<Episode, List<Episode>> episodeListMap;
+    protected Episode lastEpisode;
 
     public Scenario() {
         episodeListMap = new HashMap<>();
     }
 
     public void start() {
-        playCurrentEpisode();
+        playEpisode(currentEpisode);
     }
 
-    protected void playCurrentEpisode() {
-        if(currentEpisode == null) {
+    protected void playEpisode(Episode episode) {
+        if(episode == null) {
             System.out.println("Scenario ended");
             return;
         }
-        System.out.println("Ready to start episode: " + currentEpisode.getName());
-        final EpisodeEndState endState = (EpisodeEndState) currentEpisode.play();
+        System.out.println("Ready to start episode: " + episode.getName());
+        final EpisodeEndState endState = (EpisodeEndState) episode.play();
         System.err.println(endState);
-        currentEpisode = getNextEpisode(endState);
-        playCurrentEpisode();
+        setCurrentEpisode(getNextEpisode(endState));
+        playEpisode(currentEpisode);
     }
 
     protected void appendEpisode(Episode episode) {
@@ -35,8 +33,12 @@ public abstract class Scenario {
     }
 
     protected void addEpisodeAfter(Episode episodeBefore, Episode newEpisode) {
-        List<Episode> episodesAfter = episodeListMap.get(episodeBefore);
-        episodesAfter.add(newEpisode);
+        // If the before-episode does not exist
+        if (!episodeListMap.containsKey(episodeBefore))
+            // add it to the episode set
+            episodeListMap.put(episodeBefore, new LinkedList<Episode>());
+        else
+            episodeListMap.get(episodeBefore).add(newEpisode);
     }
 
     protected void setFirstEpisode(Episode firstEpisode) {
@@ -52,10 +54,16 @@ public abstract class Scenario {
         if(possibleNextEpisodes == null)
             return null;
         for(Episode candidateEpisode : possibleNextEpisodes) {
-            if(candidateEpisode.isAccessible(state))
+            if(candidateEpisode.isAccessible(state)) {
                 return candidateEpisode;
+            }
         }
         // check if the possible episode is accessible
         return null;
+    }
+
+    public void setCurrentEpisode(Episode newCurrentEpisode) {
+        lastEpisode = this.currentEpisode;
+        this.currentEpisode = newCurrentEpisode;
     }
 }
