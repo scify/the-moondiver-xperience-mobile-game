c90d8de16805455cf6422c69f03dd5499142a699
Paul Isaris <paulisaris@gmail.com>
2018-02-13 15:57:13 +0200

    Added basic functionality for calculator component

--- core/src/org/scify/moonwalker/app/ui/components/calculator/CalculatorComponent.java
+++ core/src/org/scify/moonwalker/app/ui/components/calculator/CalculatorComponent.java
@@ -1,4 +1,4 @@
-package org.scify.moonwalker.app.ui.components;
+package org.scify.moonwalker.app.ui.components.calculator;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Pixmap;
@@ -6,7 +6,9 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
@@ -14,25 +16,31 @@ import com.badlogic.gdx.utils.Align;
 import org.scify.moonwalker.app.helpers.GameInfo;
 import org.scify.moonwalker.app.helpers.ResourceLocator;
 
+import java.util.Arrays;
+
 public class CalculatorComponent extends Group {
 
     protected final int BUTTONS_PER_ROW = 3;
     protected final int BUTTON_PADDING = 5;
     protected final int MAIN_LABEL_HEIGHT = 200;
     protected final int TABLE_PADDING = 20;
+    protected final String MAIN_LABEL_INITIAL_TEXT = "0";
     protected Label mainLabel;
     protected String[] operatorIds = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "dot"};
-    protected String[] operationIds = {"add", "subtract", "multiply", "divide", "equal"};
+    protected String[] operationIds = {"add", "subtract", "multiply", "divide", "result"};
     protected Table containerTable;
     protected Skin skin;
     protected ResourceLocator resourceLocator;
     protected GameInfo gameInfo;
     protected Image background;
+    protected CalculatorController calculator;
 
     public CalculatorComponent(Skin skin) {
         this.skin = skin;
         this.resourceLocator = new ResourceLocator();
         this.gameInfo = GameInfo.getInstance();
+        calculator = new CalculatorController();
+        calculator.resetCalculator();
         setHeight(gameInfo.getScreenHeight());
         setWidth(gameInfo.getScreenWidth());
         initContainerTable();
@@ -41,6 +49,7 @@ public class CalculatorComponent extends Group {
         containerTable.row();
         containerTable.add(createTable(operatorIds)).bottom().center().expand();
         containerTable.add(createTable(operationIds)).bottom().center().expand();
+        // TODO Add escape button
         addActor(background);
         addActor(containerTable);
         containerTable.debug();
@@ -61,7 +70,7 @@ public class CalculatorComponent extends Group {
     }
 
     protected void createAndAddMainLabel() {
-        mainLabel = new Label("7+786-90", skin);
+        mainLabel = new Label(MAIN_LABEL_INITIAL_TEXT, skin);
         mainLabel.setHeight(MAIN_LABEL_HEIGHT);
         mainLabel.setFontScale(4);
         mainLabel.setAlignment(Align.center);
@@ -85,10 +94,16 @@ public class CalculatorComponent extends Group {
         for(String operatorId: idArray) {
             // TODO change to id when images are ready
             Drawable btnImage = new SpriteDrawable(new Sprite(new Texture(resourceLocator.getFilePath("img/calculator/1.png"))));
-            ImageButton operatorBtn = new ImageButton(btnImage);
-            operatorBtn.setName(operatorId);
-            // TODO add click listener
-            table.add(operatorBtn).size(btnSize, btnSize).pad(BUTTON_PADDING);
+            final ImageButton button = new ImageButton(btnImage);
+            button.setName(operatorId);
+            button.addListener(new ClickListener(){
+                @Override
+                public void clicked(InputEvent event, float x, float y) {
+                    super.clicked(event, x, y);
+                    parseButtonClick(button.getName());
+                }
+            });
+            table.add(button).size(btnSize, btnSize).pad(BUTTON_PADDING);
             if(btnIndex % BUTTONS_PER_ROW == 0)
                 table.row();
             btnIndex++;
@@ -98,4 +113,29 @@ public class CalculatorComponent extends Group {
     protected float singleButtonHeight(float containerHeight, String[] btnContainerArray) {
         return containerHeight / ( (float) btnContainerArray.length / BUTTONS_PER_ROW);
     }
+
+    protected void parseButtonClick(String buttonValue) {
+        // if operator clicked, parse the string to number and append to main Label
+        System.out.println(buttonValue);
+        if(buttonValue.equals("result")) {
+            mainLabel.setText(calculator.parseResult());
+            calculator.resetCalculator();
+        }
+        else {
+            if (Arrays.asList(operatorIds).contains(buttonValue)) {
+                calculator.addOperator(buttonValue);
+            } else if (Arrays.asList(operationIds).contains(buttonValue)) {
+                calculator.setOperation(buttonValue);
+            }
+            updateMainLabel();
+        }
+    }
+
+    protected void updateMainLabel() {
+        String value = calculator.getCurrentΡrepresentation();
+        if(value.equals(""))
+            value = MAIN_LABEL_INITIAL_TEXT;
+        mainLabel.setText(value);
+    }
+
 }
---
+++ core/src/org/scify/moonwalker/app/ui/components/calculator/CalculatorController.java
@@ -0,0 +1,81 @@
+package org.scify.moonwalker.app.ui.components.calculator;
+
+public class CalculatorController {
+
+    protected String operator1;
+    protected String operator2;
+    protected String operation;
+
+    public void setOperation(String operation) {
+        this.operation = operation;
+    }
+
+    public void addOperator(String operator) {
+        if(!operator.equals("dot")) {
+            if(operation.equals("") && !operator1.endsWith("."))
+                operator1 += operator;
+            else if(!operator2.endsWith("."))
+                operator2 += operator;
+        } else {
+            if(!operator2.equals(""))
+                operator2 += ".";
+            else if(!operator1.equals(""))
+                operator1 += ".";
+        }
+    }
+
+    public String parseResult() {
+        if(operator1.equals("") || operator2.equals("") || operation.equals(""))
+            return "";
+        float number1 = Float.parseFloat(operator1);
+        float number2 = Float.parseFloat(operator2);
+        float result = 0;
+        switch (operation) {
+            case "add":
+                result = number1 + number2;
+                break;
+            case "subtract":
+                result = number1 - number2;
+                break;
+            case "multiply":
+                result = number1 * number2;
+                break;
+            case "divide":
+                result = number1 / number2;
+                break;
+        }
+        String resultStr = String.valueOf(result);
+        // if result has no decimal places,
+        // just output the integer part of it
+        if(result % 1 == 0)
+            resultStr = String.valueOf((int) result);
+        return resultStr;
+    }
+
+    public String getCurrentΡrepresentation() {
+        String value = operator1 + " " + operationToSign() + " " + operator2;
+        if(operator1.equals("") && operator2.equals("") && operation.equals(""))
+            value = "";
+        return value;
+    }
+
+    public void resetCalculator() {
+        operator1 = "";
+        operator2 = "";
+        operation = "";
+    }
+
+    public String operationToSign() {
+        switch (operation) {
+            case "add":
+                return "+";
+            case "subtract":
+                return "-";
+            case "multiply":
+                return "*";
+            case "divide":
+                return "/";
+        }
+        return "";
+    }
+}
