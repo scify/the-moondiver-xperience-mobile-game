8e52ba7c54a948ef977fa199d5d699362cdc7d46
Paul Isaris <paulisaris@gmail.com>
2018-02-13 14:10:34 +0200

    Added test calculator component

--- core/src/org/scify/moonwalker/app/game/rules/episodes/RoomEpisodeRules.java
+++ core/src/org/scify/moonwalker/app/game/rules/episodes/RoomEpisodeRules.java
@@ -41,6 +41,7 @@ public class RoomEpisodeRules extends SinglePlayerRules {
             messagesLabel = new Renderable(gameInfo.getScreenWidth() - labelWidth - 20, gameInfo.getScreenHeight() / 2f - 100, labelWidth, labelHeight, "label", "messagesLabel");
             gsCurrent.addRenderable(messagesLabel);
             gsCurrent.addGameEvent(new GameEvent("UPDATE_LABEL_TEXT_UI", new HashMap.SimpleEntry<>(messagesLabel, mainLabelText)));
+            gsCurrent.addGameEvent(new GameEvent("CALCULATOR_COMPONENT"));
         }
     }
 
