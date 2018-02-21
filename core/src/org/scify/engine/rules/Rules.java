def72bcaef292d8faeca2fe6d248023a3fb4d6de
Paul Isaris <paulisaris@gmail.com>
2018-02-21 16:30:35 +0200

    Improved javadocs

--- core/src/org/scify/engine/rules/Rules.java
+++ core/src/org/scify/engine/rules/Rules.java
@@ -1,79 +1,62 @@
 package org.scify.engine.rules;
 
-import org.scify.engine.GameEngine;
-import org.scify.engine.GameEvent;
-import org.scify.engine.RenderingEngine;
-
+/**
+ * The Rules interface describes the rules of a game. It outlines objects which can act upon a game state,
+ * possibly given a set of input actions, to generate valid following states.
+ * @param <StateType> The class of the states that the Rules object is called to act upon.
+ * @param <Action> The class of actions that can trigger changes to a state handled by the Rules.
+ * @param <EndStateType> The class of the states that are generated upon the end of a game.
+ */
 public interface Rules<StateType, Action, EndStateType> {
 
     /**
-     * When the game starts, the rules are responsible of
-     * producing a game state that will include all the
-     * necessary game events that need to be dealt with
-     * in the {@link RenderingEngine} instance
+     * When the game starts, the rules are responsible to provide an initial valid state for the game.
      * @return the initial state instance
      */
     StateType getInitialState();
     /**
-     * Given a action and a current game state, this method
-     * determines the next game state that will be dealt with in
-     * the {@link RenderingEngine} instance, by modifying
-     * the passed state.
-     * @param currentState  the current game state instance
-     * @param action (nullable), an action (eg user action)
+     * Given an action and a current game state, this method
+     * determines the next game state, possibly by modifying the input state.
+     * @param currentState  the current game state instance. <b>This state object should not be considered
+     *                      immutable.</b>
+     * @param action (nullable), an action (eg user action) that affects the game state and needs to be taken
+     *               into account by the rules.
      * @return the modified state
      */
     StateType getNextState(StateType currentState, Action action);
-    boolean isGameFinished(StateType currentState);
-    boolean isGamePaused(StateType currentState);
+
     /**
-     * When the game has ended, the
-     * {@link GameEngine} instance that runs the game
-     * will trigger this method in order for the rules to
-     * clean up any resources that are unwanted
+     * Returns whether a given state indicates a finished game.
+     * @param currentState The state to evaluate.
+     * @return True if we have reached the end of the game. Otherwise, false.
+     */
+    boolean isGameFinished(StateType currentState);
+
+    /**
+     * Returns whether the game is in a paused state. This can be used to indicate that the Rules
+     * should not evaluate/change the current state, while the game is paused.
+     * @param currentState The state to evaluate.
+     * @return True if the game is paused. Otherwise, false.
+     */
+    boolean isGamePaused(StateType currentState);
+
+    /**
+     * This method should be called to dispose of any (non-trivial) resources that the Rules may
+     * have initialized.
      */
     void disposeResources();
 
     /**
-     * The {@link GameEngine} instance that runs the game
-     * asks for the eventual {@link EndStateType} that the game
-     * ended with (eg success or failure)
-     * @param currentState the current game state instance
-     * @return the state that the game ended with
+     * The Rules determines what the return end state of a game will be, provided the (last) valid
+     * game state.
+     * @param currentState the current/last game state instance
+     * @return the EndStateType instance that identifies how the game ended
      */
     EndStateType determineEndState(StateType currentState);
-    /**
-     * Removes from the state any {@link GameEvent}s that
-     * we want to be repeated if this episode is started again (resumed)
-     * For example, If we want the conversation to happen again,
-     * we should remove all conversation-relevant {@link GameEvent}s
-     * This method should be called from the {@link GameEngine} instance
-     * that runs the game, when the game has ended.
-     * @param currentState the current state instance
-     */
-    //void cleanUpState(StateType currentState);
 
     /**
-     * When the game starts, the rules might add or remove
-     * a series of events in/ from the state parameter.
-     * @param currentState the current state instance
+     * Sets the current state of the related game to a specific, new game state.
+     * @param gameState The new state.
      */
-    void gameStartedEvents(StateType currentState);
-
-    /**
-     *
-     * When the game ends, the rules might add or remove
-     * a series of events in/ from the state parameter.
-     * @param currentState the current state instance
-     */
-    void gameEndedEvents(StateType currentState);
-
-    /**
-     * When the game is resumed, the rules might add or remove
-     * a series of events in/ from the state parameter.
-     * @param currentState the current state instance
-     */
-    void gameResumedEvents(StateType currentState);
-
-    void setInitialState(StateType type);
+    void setInitialState(StateType gameState);
 }
