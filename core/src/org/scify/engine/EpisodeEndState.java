9fd9761f181be2a8aa72ee699538ad7decdbd783
Paul Isaris <paulisaris@gmail.com>
2018-02-22 14:32:07 +0200

    Comments

--- core/src/org/scify/engine/EpisodeEndState.java
+++ core/src/org/scify/engine/EpisodeEndState.java
@@ -6,7 +6,13 @@ package org.scify.engine;
  * as well as carrying the last instance of the {@link GameState} game state
  */
 public class EpisodeEndState extends EpisodeState {
+
+    /**
+     * The code describes "how" the episode ended.
+     * For example, the episode may end with a "success" or "failure" status
+     */
     protected EpisodeEndStateCode endStateCode;
+
     public EpisodeEndState(EpisodeEndStateCode endStateCode, GameState gameState) {
         super(gameState);
         this.endStateCode = endStateCode;
--- core/src/org/scify/engine/EpisodeState.java
+++ core/src/org/scify/engine/EpisodeState.java
@@ -1,6 +1,13 @@
 package org.scify.engine;
 
+/**
+ * This class describes the state in which an {@link Episode} is at any given time.
+ */
 public class EpisodeState {
+
+    /**
+     * The state of the game that the episode is currently handling.
+     */
     protected GameState gameState;
 
     public EpisodeState(GameState gameState) {
