3ef74a37f01293d2cfd7aae870f61cdb4ada892e
Paul Isaris <paulisaris@gmail.com>
2018-02-22 14:54:42 +0200

    Removed unused code

--- core/src/org/scify/moonwalker/app/ui/ThemeController.java
+++ core/src/org/scify/moonwalker/app/ui/ThemeController.java
@@ -25,7 +25,7 @@ public class ThemeController {
     public void initFontAndSkin() {
         FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
         parameter.characters=FreeTypeFontGenerator.DEFAULT_CHARS + "α  β  γ  δ  ε  ζ  η  θ  ι  κ  λ  μ  ν  ξ  ο  π  ρ  σ  τ  υ  ϕ  χ  ψ  ω ς Α  Β  Γ  Δ  Ε  Ζ  Η  Θ  Ι  Κ  Λ  Μ  Ν  Ξ  Ο  Π  Ρ  Σ  Τ  Υ  Φ  Χ  Ψ  Ω ά έ ή ί ό ύ ώ Ά Έ Ή Ί Ό Ύ Ώ";
-        parameter.size = getPixelSizeForFontSize(22);
+        parameter.size = (int) gameInfo.pixelsWithDensity(22);
 
         FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal(resourceLocator.getFilePath("fonts/Anonymous_Pro.ttf")));
         font = generator.generateFont(parameter);
@@ -35,19 +35,6 @@ public class ThemeController {
         skin.addRegions(new TextureAtlas(Gdx.files.internal(resourceLocator.getFilePath("fonts/uiskin.atlas"))));
         skin.load(Gdx.files.internal(resourceLocator.getFilePath("fonts/uiskin.json")));
         generator.dispose();
-    }
-
-    private BitmapFont createFont(FreeTypeFontGenerator generator, float fontSize) {
-        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
-        float screenDPI = 160.0f * Gdx.graphics.getDensity();
-        int pixelSize = (int)(fontSize * screenDPI / 160.0f); // Reference size based on 96 DPI screen
-        parameter.size = pixelSize;
-        //Gdx.app.log(TAG, "Font size: "+pixelSize+"px");
-        return generator.generateFont(parameter);
-    }
-
-    protected int getPixelSizeForFontSize(float fontSize) {
-        return (int) gameInfo.pixelsWithDensity(fontSize);
     }
 
     public BitmapFont getFont() {
