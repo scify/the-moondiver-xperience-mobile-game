0e5586b7085d1e2ddbfbd07dfb0b70f3821065bc
Paul Isaris <paulisaris@gmail.com>
2018-02-26 11:40:32 +0200

    Added addUserAction method to interface

--- core/src/org/scify/engine/UserInputHandler.java
+++ core/src/org/scify/engine/UserInputHandler.java
@@ -6,4 +6,5 @@ public interface UserInputHandler extends ContactListener {
     UserAction getNextUserAction();
     void addUserActionForRenderable(Renderable r, Object payload);
     void disposeResources();
+    void addUserAction(UserAction userAction);
 }
