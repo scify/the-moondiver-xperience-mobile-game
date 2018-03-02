e8516d587920f96b92e9cd8abdd7112b1c1550c0
Paul Isaris <paulisaris@gmail.com>
2018-03-02 10:44:54 +0200

    Added handling for spaceship charger episode

--- core/src/org/scify/moonwalker/app/game/rules/episodes/CockpitRules.java
+++ core/src/org/scify/moonwalker/app/game/rules/episodes/CockpitRules.java
@@ -11,8 +11,12 @@ public class CockpitRules extends BaseEpisodeRules {
     @Override
     protected void handleUserAction(GameState gsCurrent, UserAction userAction) {
         switch (userAction.getActionCode()) {
-            case FINISH_EPISODE:
-                cockpit.setRemainingEnergyValue("1124564");
+            case BACK:
+                gameEndedEvents(gsCurrent);
+                break;
+            case CHARGE_SPACESHIP_EPISODE:
+                gsCurrent.addGameEvent(new GameEvent("SPACESHIP_CHARGER_EPISODE_STARTED", null, this));
+                gameEndedEvents(gsCurrent);
                 break;
             default:
                 super.handleUserAction(gsCurrent, userAction);
@@ -37,6 +41,10 @@ public class CockpitRules extends BaseEpisodeRules {
         EpisodeEndState endStateFromParent = super.determineEndState(currentState);
         if(endStateFromParent != null)
             return endStateFromParent;
+        else if(currentState.eventsQueueContainsEventOwnedBy("MAP_EPISODE_STARTED", this))
+            return new EpisodeEndState(EpisodeEndStateCode.MAP_EPISODE_STARTED, cleanUpState(currentState));
+        else if(currentState.eventsQueueContainsEventOwnedBy("SPACESHIP_CHARGER_EPISODE_STARTED", this))
+            return new EpisodeEndState(EpisodeEndStateCode.SPACESHIP_CHARGER_EPISODE_STARTED, cleanUpState(currentState));
         return new EpisodeEndState(EpisodeEndStateCode.EPISODE_FINISHED_FAILURE, cleanUpState(currentState));
     }
     
@@ -44,15 +52,17 @@ public class CockpitRules extends BaseEpisodeRules {
         cockpit = new CockpitRenderable(0,0, gameInfo.getScreenWidth(),
                 gameInfo.getScreenHeight(), "cockpit", "cockpit");
         cockpit.setImgPath("img/cockpit.png");
-        ActionButton navigationBtn = createCockpitButton("navigation_button", "img/navigation.png", UserActionCode.FINISH_EPISODE);
-        ActionButton vesselBtn = createCockpitButton("vessel_button", "img/rocket.png", UserActionCode.FINISH_EPISODE);
-        ActionButton mapBtn = createCockpitButton("map_button", "img/map.png", UserActionCode.MAP);
+        ActionButton launchBtn = createCockpitButton("navigation_button", "img/launch.png", UserActionCode.FINISH_EPISODE);
+        ActionButton spaceshipPartsButton = createCockpitButton("spaceship_parts_button", "img/rocket.png", UserActionCode.SPACESHIP_PARTS_EPISODE);
+        ActionButton mapBtn = createCockpitButton("map_button", "img/map.png", UserActionCode.MAP_EPISODE);
         ActionButton contactBtn = createCockpitButton("contact_button", "img/contact.png", UserActionCode.FINISH_EPISODE);
+        ActionButton chargeBtn = createCockpitButton("charge_button", "img/battery.png", UserActionCode.CHARGE_SPACESHIP_EPISODE);
 
-        cockpit.setNavigationButton(navigationBtn);
-        cockpit.setVesselButton(vesselBtn);
+        cockpit.setLaunchButton(launchBtn);
+        cockpit.setSpaceshipPartsButton(spaceshipPartsButton);
         cockpit.setMapButton(mapBtn);
         cockpit.setContactButton(contactBtn);
+        cockpit.setChargeButton(chargeBtn);
 
         currentState.addRenderable(cockpit);
     }
