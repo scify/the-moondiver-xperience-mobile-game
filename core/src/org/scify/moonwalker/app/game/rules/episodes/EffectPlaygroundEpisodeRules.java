8e687b5e8fc38a6e6d20e20dd7c8373cfb76189f
George Giannakopoulos <ggianna@scify.org>
2018-05-02 20:49:30 +0300

    - Created individual methods to test different effects/renderables.

--- core/src/org/scify/moonwalker/app/game/rules/episodes/EffectPlaygroundEpisodeRules.java
+++ core/src/org/scify/moonwalker/app/game/rules/episodes/EffectPlaygroundEpisodeRules.java
@@ -6,7 +6,9 @@ import org.scify.engine.UserAction;
 import org.scify.engine.renderables.ActionButtonRenderable;
 import org.scify.engine.renderables.ImageRenderable;
 import org.scify.engine.renderables.Renderable;
-import org.scify.engine.renderables.effects.RotateEffect;
+import org.scify.engine.renderables.TextLabelRenderable;
+import org.scify.engine.renderables.effects.*;
+import org.scify.engine.renderables.effects.libgdx.PointRouteFadeEffectLGDX;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,98 +27,21 @@ public class EffectPlaygroundEpisodeRules extends SimpleTimedImageEpisodeRules {
         }
 
         if (aTest == null) {
-            aTest = new ActionButtonRenderable("rotatable_text_button", "ID_OK");
-            ((ActionButtonRenderable) aTest).setUserAction(new UserAction("test"));
-//            aTest.setxPos(250);
-//            aTest.setyPos(250);
-//            aTest.setWidth(250);
-//            aTest.setHeight(100);
-//            aTest.addEffect(new FadeEffect(1.0, 0.3, 5000.0)).addEffect(new
-//                    RotateEffect(0.0, 360.0, 8000.0));
-////            aTest.apply(new SlideEffect(aTest.getxPos(), aTest.getyPos(), 270.0, 1000));
-//            ((ActionButtonRenderable)aTest).setTitle("Normal (parallel)");
-//            currentState.addRenderable(aTest);
-//
-//            aTest2 = new TextLabelRenderable("rotatable_label", "ID_ANOTHER");
-//            aTest2.setxPos(500);
-//            aTest2.setyPos(100);
-//            ((TextLabelRenderable)aTest2).setLabel("Consequtive");
-//
-//            EffectSequence elList = new EffectSequence();
-//            elList.addEffect(new FadeEffect(1.0, 0.3, 1000.0));
-//            elList.addEffect(new RotateEffect(0.0, 720.0, 1000.0));
-//            elList.addEffect(new VisibilityEffect(false));
-//            elList.addEffect(new DelayEffect(2000));
-//            elList.addEffect(new VisibilityEffect(true));
-//            elList.addEffect(new FadeEffect(0.3, 1.0, 2000.0));
-//            elList.addEffect(new RotateEffect(100.0, 0.0, 1000.0));
-//            elList.addEffect((new BounceEffect(0.0, 100.0, 3000.0)));
-//            elList.addEffect(new FunctionEffect(new Runnable() {
-//                        @Override
-//                        public void run() {
-//                            System.err.println("Hello!");
-//                        }
-//                    }));
-//            aTest2.addEffect(elList);
-//            currentState.addRenderable(aTest2);
-////
-//            aTest3 = new TextLabelRenderable("rotatable_label", "ID_3RD");
-//            aTest3.setxPos(200);
-//            aTest3.setyPos(50);
-//            ((TextLabelRenderable)aTest3).setLabel("Consequtive Parallels");
-////
-//            // Point route with many points alternative
-////            List<Renderable> lPoints = new ArrayList<>();
-////            for (int iCnt=0; iCnt < 10; iCnt++) {
-////                ImageRenderable rPl = new ImageRenderable(10,10,50,50,"point" + String.valueOf(iCnt), "img/close.png");
-////                rPl.setxPos(50 + 50 * iCnt);
-////                rPl.setyPos((float)(50.0 + 100.0 * Math.sin(2.0 * Math.PI * iCnt / 8.0)));
-////                rPl.setVisible(false);
-////
-////                lPoints.add(rPl);
-////                currentState.addRenderable(rPl);
-////            }
-////
-////            PointRouteFadeEffectLGDX pRoute = new PointRouteFadeEffectLGDX(2000, lPoints);
+            getNormalParallelRenderable(currentState);
 
-            // Point route with single item type alternative
-            List<Vector2> lvPoints = new ArrayList<>();
-            ImageRenderable rPl = new ImageRenderable(10,10,50,50,"pointType", "img/close.png");
-            rPl.setVisible(true);
-//            // Create points
-//            for (int iCnt=0; iCnt < 5; iCnt++) {
-//                Vector2 vCur = new Vector2();
-//                vCur.set(50 + 50 * iCnt, (float)(50.0 + 20 * Math.sin(2.0 * Math.PI * (float)iCnt / 100.0)));
-//                lvPoints.add(vCur);
-//            }
-//            PointRouteSinglePointTypeEffect pRoute = new PointRouteSinglePointTypeEffect(lvPoints, 3.0, 2.0, 5000);
-//            rPl.addEffect(pRoute);
-            RotateEffect rEffect = new RotateEffect(0.0, 360.0, 6000);
-            rEffect.setOriginPoint(15.0, 15.0);
-            rPl.addEffect(rEffect);
+            getConsequtiveRenderable(currentState);
+////
+            getConsequtiveParallelsRenderable(currentState);
 
-            currentState.addRenderable(rPl);
+//
+            getRouteWithManyPointsRenderablesAndEffect(currentState);
+            getPointRouteWithSingleItemTypeRenderable(currentState);
 
-//            EffectSequence elConPar = new EffectSequence();
-//
-//            ParallelEffectList pelList = new ParallelEffectList();
-//            pelList.addEffect(new VisibilityEffect(true));
-//            pelList.addEffect(new FadeEffect(1.0, 0.5, 2000.0));
-//            pelList.addEffect(new RotateEffect(0.0, 180.0, 2000.0));
-//            pelList.addEffect(new BounceEffect(50,0,5000.0));
-//
-//            Effect eDelay = new DelayEffect(1000);
-////
-//            ParallelEffectList pelList2 = new ParallelEffectList();
-//            pelList2.addEffect(new FadeEffect(0.5, 1.0, 2000.0));
-//            pelList2.addEffect(new RotateEffect(180.0, 0.0, 2000.0));
-////
-//            elConPar.addEffect(pelList);
-//            elConPar.addEffect(eDelay);
-//            elConPar.addEffect(pelList2);
-////
-//////            aTest3.addEffect(elConPar);
-//////            currentState.addRenderable(aTest3);
+
+//            RotateEffect rEffect = new RotateEffect(0.0, 360.0, 6000);
+//            rEffect.setOriginPoint(15.0, 15.0);
+//            rPl.addEffect(rEffect);
+
 ////
 //            Renderable rCoin = new ImageRenderable(50,50,100,100,"player", "img/coin.png");
 //            rCoin.setVisible(false);
@@ -129,6 +54,105 @@ public class EffectPlaygroundEpisodeRules extends SimpleTimedImageEpisodeRules {
         }
 
         super.episodeStartedEvents(currentState);
+    }
+
+    private void getPointRouteWithSingleItemTypeRenderable(GameState currentState) {
+        // Point route with single item type alternative
+        List<Vector2> lvPoints = new ArrayList<>();
+        ImageRenderable rPl = new ImageRenderable(10,10,50,50,"pointType", "img/close.png");
+        rPl.setVisible(false);
+        // Create points
+        for (int iCnt=0; iCnt < 5; iCnt++) {
+            Vector2 vCur = new Vector2();
+            vCur.set(50 + 50 * iCnt, (float)(50.0 + 20 * Math.sin(2.0 * Math.PI * (float)iCnt / 100.0)));
+            lvPoints.add(vCur);
+        }
+        PointRouteSinglePointTypeEffect pRoute = new PointRouteSinglePointTypeEffect(lvPoints, 3.0, 2.0, 5000);
+        rPl.addEffect(pRoute);
+
+        currentState.addRenderable(rPl);
+    }
+
+    private void getRouteWithManyPointsRenderablesAndEffect(GameState currentState) {
+        // Point route with many points alternative
+        List<Renderable> lPoints = new ArrayList<>();
+        for (int iCnt=0; iCnt < 10; iCnt++) {
+            ImageRenderable rBasePoint = new ImageRenderable(10,10,50,50,"point" + String.valueOf(iCnt), "img/close.png");
+            rBasePoint.setxPos(50 + 50 * iCnt);
+            rBasePoint.setyPos((float)(50.0 + 100.0 * Math.sin(2.0 * Math.PI * iCnt / 8.0)));
+            rBasePoint.setVisible(false);
+
+            lPoints.add(rBasePoint);
+            currentState.addRenderable(rBasePoint);
+        }
+
+        PointRouteFadeEffectLGDX pRoute = new PointRouteFadeEffectLGDX(2000, lPoints);
+    }
+
+    private void getConsequtiveRenderable(GameState currentState) {
+        aTest2 = new TextLabelRenderable("rotatable_label", "ID_ANOTHER");
+        aTest2.setxPos(500);
+        aTest2.setyPos(100);
+        ((TextLabelRenderable)aTest2).setLabel("Consequtive");
+
+        EffectSequence elList = new EffectSequence();
+        elList.addEffect(new FadeEffect(1.0, 0.3, 1000.0));
+        elList.addEffect(new RotateEffect(0.0, 720.0, 1000.0));
+        elList.addEffect(new VisibilityEffect(false));
+        elList.addEffect(new DelayEffect(2000));
+        elList.addEffect(new VisibilityEffect(true));
+        elList.addEffect(new FadeEffect(0.3, 1.0, 2000.0));
+        elList.addEffect(new RotateEffect(100.0, 0.0, 1000.0));
+        elList.addEffect((new BounceEffect(0.0, 100.0, 3000.0)));
+        elList.addEffect(new FunctionEffect(new Runnable() {
+                    @Override
+                    public void run() {
+                        System.err.println("Hello!");
+                    }
+                }));
+        aTest2.addEffect(elList);
+        currentState.addRenderable(aTest2);
+    }
+
+    private void getNormalParallelRenderable(GameState currentState) {
+        aTest = new ActionButtonRenderable("rotatable_text_button", "ID_OK");
+        ((ActionButtonRenderable) aTest).setUserAction(new UserAction("test"));
+        aTest.setxPos(250);
+        aTest.setyPos(250);
+        aTest.setWidth(250);
+        aTest.setHeight(100);
+        aTest.addEffect(new FadeEffect(1.0, 0.3, 5000.0)).addEffect(new
+                RotateEffect(0.0, 360.0, 8000.0));
+//            aTest.apply(new SlideEffect(aTest.getxPos(), aTest.getyPos(), 270.0, 1000));
+        ((ActionButtonRenderable)aTest).setTitle("Normal (parallel)");
+        currentState.addRenderable(aTest);
+    }
+
+    private void getConsequtiveParallelsRenderable(GameState currentState) {
+        aTest3 = new TextLabelRenderable("rotatable_label", "ID_3RD");
+        aTest3.setxPos(200);
+        aTest3.setyPos(50);
+        ((TextLabelRenderable)aTest3).setLabel("Consequtive Parallels");
+        EffectSequence elConPar = new EffectSequence();
+
+        ParallelEffectList pelList = new ParallelEffectList();
+        pelList.addEffect(new VisibilityEffect(true));
+        pelList.addEffect(new FadeEffect(1.0, 0.5, 2000.0));
+        pelList.addEffect(new RotateEffect(0.0, 180.0, 2000.0));
+        pelList.addEffect(new BounceEffect(50,0,5000.0));
+
+        Effect eDelay = new DelayEffect(1000);
+//
+        ParallelEffectList pelList2 = new ParallelEffectList();
+        pelList2.addEffect(new FadeEffect(0.5, 1.0, 2000.0));
+        pelList2.addEffect(new RotateEffect(180.0, 0.0, 2000.0));
+//
+        elConPar.addEffect(pelList);
+        elConPar.addEffect(eDelay);
+        elConPar.addEffect(pelList2);
+//
+        aTest3.addEffect(elConPar);
+        currentState.addRenderable(aTest3);
     }
 
     @Override
