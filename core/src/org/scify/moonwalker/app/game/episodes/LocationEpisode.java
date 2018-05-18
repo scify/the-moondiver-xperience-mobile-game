7ff8cb5019015390ee5925f013e3d8b212fac982
Paul Isaris <paulisaris@gmail.com>
2018-05-18 10:36:35 +0300

    Added basic class for Location Episodes

--- core/src/org/scify/moonwalker/app/game/episodes/LocationEpisode.java
+++ core/src/org/scify/moonwalker/app/game/episodes/LocationEpisode.java
@@ -3,7 +3,7 @@ package org.scify.moonwalker.app.game.episodes;
 import org.scify.engine.EpisodeWithEndState;
 import org.scify.engine.rules.Rules;
 
-public class LocationEpisode extends EpisodeWithEndState {
+public abstract class LocationEpisode extends EpisodeWithEndState {
     public LocationEpisode(Rules rules) {
         super(rules);
     }
