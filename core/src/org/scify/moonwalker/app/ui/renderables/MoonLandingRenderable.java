63a90160ed22cc6878fa7779935054fdbc638008
Paul Isaris <paulisaris@gmail.com>
2018-05-21 14:59:34 +0300

    Refactored resource directories

--- core/src/org/scify/moonwalker/app/ui/renderables/MoonLandingRenderable.java
+++ core/src/org/scify/moonwalker/app/ui/renderables/MoonLandingRenderable.java
@@ -1,5 +1,7 @@
 package org.scify.moonwalker.app.ui.renderables;
 
+import org.scify.moonwalker.app.game.SelectedPlayer;
+
 public class MoonLandingRenderable extends ChattableRenderable {
 
     // the following two static variables need to be set BEFORE the Class instance
@@ -8,21 +10,24 @@ public class MoonLandingRenderable extends ChattableRenderable {
     public static String BG_IMG_PATH;
     public static String AUDIO_PATH = "audio/episode_moon_landing/";
     public static String CONVERSATION_BG_IMG_PATH;
-    public static String AUDIO_BG;
+    public static String AUDIO_BG_PATH;
     //Audio
 //    public final static String FOREST_AUDIO_PATH = "audio/episode_forest/bg.mp3";
 //    public final static String BORING_MUSIC_AUDIO_PATH = "audio/episode_forest/boring_music.mp3";
 //    public final static String MOBILE_AUDIO_PATH = "audio/message.mp3";
 //    public final static String MAINMENU_AUDIO_PATH = "audio/episode_main_menu/bg.mp3";
 
-    public MoonLandingRenderable(float xPos, float yPos, float width, float height, String id) {
+    public MoonLandingRenderable(float xPos, float yPos, float width, float height, String id, String playerSex, boolean isGameFullySuccessfullyCompleted) {
         super(xPos, yPos, width, height, id, id, BG_IMG_PATH);
-        CONVERSATION_BG_IMG_PATH = IMG_PATH + "conversation_bg.png";
-        AUDIO_BG = AUDIO_PATH + "bg.mp3";
+
     }
 
-    public String calculateBgImgPath() {
-        return null;
+    public static void calculateResPaths(String playerSex, boolean isGameFullySuccessfullyCompleted) {
+        IMG_PATH += isGameFullySuccessfullyCompleted ? "full_success/" : "simple_success/";
+        BG_IMG_PATH = playerSex.equals(SelectedPlayer.boy) ? MoonLandingRenderable.IMG_PATH + "boy_bg.png" : MoonLandingRenderable.IMG_PATH + "girl_bg.png";
+        CONVERSATION_BG_IMG_PATH = IMG_PATH + "conversation_bg.png";
+        AUDIO_PATH += isGameFullySuccessfullyCompleted ? "full_success/" : "simple_success/";
+        AUDIO_BG_PATH = AUDIO_PATH + "bg.mp3";
     }
 
 }
