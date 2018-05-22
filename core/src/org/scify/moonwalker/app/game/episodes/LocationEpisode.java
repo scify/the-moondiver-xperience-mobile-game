4472188c53bde5637eb6eb13b6e6c8ff28de9975
Paul Isaris <paulisaris@gmail.com>
2018-05-22 10:52:54 +0300

    Made class concrete

--- core/src/org/scify/moonwalker/app/game/episodes/LocationEpisode.java
+++ core/src/org/scify/moonwalker/app/game/episodes/LocationEpisode.java
@@ -3,7 +3,7 @@ package org.scify.moonwalker.app.game.episodes;
 import org.scify.engine.EpisodeWithEndState;
 import org.scify.engine.rules.Rules;
 
-public abstract class LocationEpisode extends EpisodeWithEndState {
+public class LocationEpisode extends EpisodeWithEndState {
     public LocationEpisode(Rules rules) {
         super(rules);
     }
