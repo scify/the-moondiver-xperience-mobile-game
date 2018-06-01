fc0b222b367f630d112e006bd5c6f138b1c3d52c
Paul Isaris <paulisaris@gmail.com>
2018-06-01 11:25:55 +0300

    Moved class

--- core/src/org/scify/moonwalker/app/ui/UnsupportedRenderableTypeException.java
+++
@@ -1,7 +0,0 @@
-package org.scify.moonwalker.app.ui;
-
-public class UnsupportedRenderableTypeException extends UnsupportedOperationException {
-    public UnsupportedRenderableTypeException(String s) {
-        super(s);
-    }
-}
