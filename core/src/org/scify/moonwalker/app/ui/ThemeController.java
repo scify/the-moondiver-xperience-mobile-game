c2fc1126eeff914fb7d71e7519d7f09c68febc94
George Giannakopoulos <ggianna@scify.org>
2018-11-27 14:10:41 +0200

    - Fixes in dialogues ( episode_forest.json, episode_room.json ). - Added correct and wrong skin resources and atlases. - ThemeController.java: Support 3 types of skins.

--- core/src/org/scify/moonwalker/app/ui/ThemeController.java
+++ core/src/org/scify/moonwalker/app/ui/ThemeController.java
@@ -14,9 +14,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ThemeController {
+    public static final String SKIN_DEFAULT = "fonts/uiskin.atlas";
+    public static final String SKIN_CORRECT = "fonts/uiskin_correctAnswer.atlas";
+    public static final String SKIN_WRONG = "fonts/uiskin_wrongAnswer.atlas";
 
     private Skin defaultSkin;
     protected Skin wrongAnswerSkin;
+    protected Skin correctAnswerSkin;
+
     private ResourceLocator resourceLocator;
     private AppInfo appInfo;
     private static ThemeController instance;
@@ -35,14 +40,18 @@ public class ThemeController {
         this.appInfo = AppInfo.getInstance();
         fonts = new HashMap<>();
         defaultSkin = new Skin();
+        setDefaultFontOfSkin(defaultSkin, SKIN_DEFAULT);
+
         wrongAnswerSkin = new Skin();
-        setDefaultFontOfSkin(defaultSkin, true);
-        setDefaultFontOfSkin(wrongAnswerSkin, false);
+        setDefaultFontOfSkin(wrongAnswerSkin, SKIN_WRONG);
+
+        correctAnswerSkin = new Skin();
+        setDefaultFontOfSkin(correctAnswerSkin, SKIN_CORRECT);
     }
 
-    protected FontData initFontAndSkin(Skin skin, int fontSize, String fontId, boolean defaultSkin) {
+    protected FontData initFontAndSkin(Skin skin, int fontSize, String fontId, String sSkin) {
         FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
-        parameter.characters = FreeTypeFontGenerator.DEFAULT_CHARS + "α  β  γ  δ  ε  ζ  η  θ  ι  κ  λ  μ  ν  ξ  ο  π  ρ  σ  τ  υ  φ  χ  ψ  ω  ς  Α  Β  Γ  Δ  Ε  Ζ  Η  Θ  Ι  Κ  Λ  Μ  Ν  Ξ  Ο  Π  Ρ  Σ  Τ  Υ  Φ  Χ  Ψ  Ω ϊ ά έ ή ί ό ύ ώ ΐ ΰ Ά Έ Ή Ί Ό Ύ Ώ";
+        parameter.characters = FreeTypeFontGenerator.DEFAULT_CHARS + "α  β  γ  δ  ε  ζ  η  θ  ι  κ  λ  μ  ν  ξ  ο  π  ρ  σ  τ  υ  φ  χ  ψ  ω  ς  Α  Β  Γ  Δ  Ε  Ζ  Η  Θ  Ι  Κ  Λ  Μ  Ν  Ξ  Ο  Π  Ρ  Σ  Τ  Υ  Φ  Χ  Ψ  Ω ϊ ά έ ή ί ό ύ ώ ΐ ΰ Ά Έ Ή Ί Ό Ύ Ώ ' \"";
         int width = appInfo.getScreenWidth();
         float ratio = width / 731f;
         parameter.size = (int) (ratio * fontSize);
@@ -66,10 +75,7 @@ public class ThemeController {
 
         skin.add("default-font", font, BitmapFont.class);
         TextureAtlas textureAtlas;
-        if (defaultSkin)
-            textureAtlas = new TextureAtlas(Gdx.files.internal(resourceLocator.getFilePath("fonts/uiskin.atlas")));
-        else
-            textureAtlas = new TextureAtlas(Gdx.files.internal(resourceLocator.getFilePath("fonts/uiskin_wrongAnswer.atlas")));
+        textureAtlas = new TextureAtlas(Gdx.files.internal(resourceLocator.getFilePath(sSkin)));
 
         skin.addRegions(textureAtlas);
         skin.load(Gdx.files.internal(resourceLocator.getFilePath("fonts/uiskin.json")));
@@ -78,18 +84,18 @@ public class ThemeController {
         return new FontData(textureAtlas, font);
     }
 
-    public BitmapFont getFont(int fontSize, String fontId, boolean defaultSkin) {
+    public BitmapFont getFont(int fontSize, String fontId, String skin) {
         if (fonts.containsKey(fontId)) {
             Map<Integer, FontData> sizeToFont = fonts.get(fontId);
             if (sizeToFont.containsKey(fontSize))
                 return sizeToFont.get(fontSize).getFont();
             else {
-                FontData font = initFontAndSkin(new Skin(), fontSize, fontId, defaultSkin);
+                FontData font = initFontAndSkin(new Skin(), fontSize, fontId, skin);
                 sizeToFont.put(fontSize, font);
                 return font.getFont();
             }
         } else {
-            FontData font = initFontAndSkin(new Skin(), fontSize, fontId, defaultSkin);
+            FontData font = initFontAndSkin(new Skin(), fontSize, fontId, skin);
             Map<Integer, FontData> sizeToFont = new HashMap<>();
             sizeToFont.put(fontSize, font);
             fonts.put(fontId, sizeToFont);
@@ -97,15 +103,15 @@ public class ThemeController {
         }
     }
 
-    protected void setDefaultFontOfSkin(Skin skin, boolean isDefaultSkin) {
-        FontData font = initFontAndSkin(skin, 20, "dialog", isDefaultSkin);
+    protected void setDefaultFontOfSkin(Skin skin, String sSkinType) {
+        FontData font = initFontAndSkin(skin, 20, "dialog", sSkinType);
         Map<Integer, FontData> sizeToFont = new HashMap<>();
         sizeToFont.put(20, font);
         fonts.put("dialog", sizeToFont);
     }
 
     public BitmapFont getFont() {
-        return getFont(20, "dialog", true);
+        return getFont(20, "dialog", SKIN_DEFAULT);
     }
 
     public Skin getDefaultSkin() {
